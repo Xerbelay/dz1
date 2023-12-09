958c590776beb342b8dfc08bf02320f2c4fdef69
Xerbelay <148629656+Xerbelay@users.noreply.github.com>
2023-12-09 12:44:05 +0300

    dz

---
+++ dz/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <p v-if="age <= 18">подросток</p>
+  <p v-if="age > 18 && age <= 25">молодой человек</p>
+  <p v-if="age > 25">мужчина</p>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      age: +prompt("введите ваш возраст")
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
